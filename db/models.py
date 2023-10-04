54b38fadbf10279d43704f1fd56acef307eb4760
Agshin Yolcuyev <agshin@google.com>
2023-10-04 19:44:47 +0200

    update sql orm

--- db/models.py
+++ db/models.py
@@ -1,5 +1,8 @@
+from datetime import datetime
+
 from sqlalchemy import Boolean, Column, ForeignKey, Integer, String, Double, DateTime, UniqueConstraint
 from sqlalchemy.orm import relationship
+from sqlalchemy.orm import Mapped, mapped_column
 
 from .database import Base
 
@@ -7,31 +10,31 @@ from .database import Base
 class Currencies(Base):
     __tablename__ = "currencies"
 
-    id = Column(Integer, primary_key=True, index=True,autoincrement=True)
-    name = Column(String, unique=True, index=True)
+    id: Mapped[int]  = mapped_column( primary_key=True, autoincrement=True)
+    name: Mapped[str] = mapped_column(unique=True)
 
 
 class SubCurrencies(Base):
     __tablename__ = "sub_currencies"
 
-    id = Column(Integer, primary_key=True, index=True,autoincrement=True)
-    name = Column(String, index=True)
-    cur_id = Column(Integer, ForeignKey("currencies.id"))
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    name: Mapped[str]
+    cur_id: Mapped[int] = mapped_column(ForeignKey("currencies.id"))
 
 
 class Addresses(Base):
     __tablename__ = "addresses"
 
-    id = Column(Integer, primary_key=True, index=True,autoincrement=True)
-    address = Column(String,unique=True ,index=True)
+    id: Mapped[int] = mapped_column(primary_key=True,autoincrement=True)
+    address: Mapped[str] = mapped_column(unique=True)
 
 
 
 class Statistic(Base):
     __tablename__ = "statistic"
 
-    id = Column(Integer, primary_key=True, index=True,autoincrement=True)
-    address_id = Column(Integer, ForeignKey("addresses.id"))
-    sub_currency_id = Column(Integer, ForeignKey("sub_currencies.id"))
-    balance = Column(Double)
-    updated_at = Column(DateTime)
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, index=True,autoincrement=True)
+    address_id: Mapped[int] = mapped_column(Integer, ForeignKey("addresses.id"))
+    sub_currency_id: Mapped[int] = mapped_column(Integer, ForeignKey("sub_currencies.id"))
+    balance: Mapped[float]
+    updated_at: Mapped[datetime]

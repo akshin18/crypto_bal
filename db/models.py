0609d55a34ad8203b9c1e852109db352906ce721
Agshin Yolcuyev <agshin@google.com>
2023-10-04 20:01:47 +0200

    update alembic

--- db/models.py
+++ db/models.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 
-from sqlalchemy import Boolean, Column, ForeignKey, Integer, String, Double, DateTime, UniqueConstraint
+from sqlalchemy import ForeignKey
 from sqlalchemy.orm import relationship
 from sqlalchemy.orm import Mapped, mapped_column
 
@@ -10,7 +10,7 @@ from .database import Base
 class Currencies(Base):
     __tablename__ = "currencies"
 
-    id: Mapped[int]  = mapped_column( primary_key=True, autoincrement=True)
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
     name: Mapped[str] = mapped_column(unique=True)
 
 
@@ -18,23 +18,27 @@ class SubCurrencies(Base):
     __tablename__ = "sub_currencies"
 
     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
-    name: Mapped[str]
     cur_id: Mapped[int] = mapped_column(ForeignKey("currencies.id"))
+    name: Mapped[str]
 
 
 class Addresses(Base):
     __tablename__ = "addresses"
 
-    id: Mapped[int] = mapped_column(primary_key=True,autoincrement=True)
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
     address: Mapped[str] = mapped_column(unique=True)
+
+    statistic: Mapped["Statistic"] = relationship(back_populates="address")
 
 
 
 class Statistic(Base):
     __tablename__ = "statistic"
 
-    id: Mapped[int] = mapped_column(Integer, primary_key=True, index=True,autoincrement=True)
-    address_id: Mapped[int] = mapped_column(Integer, ForeignKey("addresses.id"))
-    sub_currency_id: Mapped[int] = mapped_column(Integer, ForeignKey("sub_currencies.id"))
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    address_id: Mapped[int] = mapped_column(ForeignKey("addresses.id"))
+    sub_currency_id: Mapped[int] = mapped_column(ForeignKey("sub_currencies.id"))
     balance: Mapped[float]
     updated_at: Mapped[datetime]
+
+    address: Mapped["Addresses"] = relationship(back_populates="statistic")
